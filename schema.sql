bde499612bc80dd93ee6f1f66e2593881fed621f
Mike Prap <michaelpraplaski@yahoo.com>
2019-10-07 23:15:55 -0400

    added more products

--- schema.sql
+++ schema.sql
@@ -20,3 +20,30 @@ SELECT * FROM items
 
 INSERT INTO items (product_name, department_name, price, stock_quanity)
 VALUES ("Corvette", "car", 50000.00, 5);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("Ice Cream", "food", 5.00, 500);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("fifa20", "video game", 60.00, 100);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("porche", "car", 80000.00, 5);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("Dark Souls 3", "Video game", 30.00, 100);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("bike", "sports", 300.00, 10);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("Macbook pro", "electronics", 2000.00, 20);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("surface pro", "electronics", 1500.00, 20);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("cigarettes", "tobaco", 8.00, 1000);
+
+INSERT INTO items (product_name, department_name, price, stock_quanity)
+VALUES ("Corvette", "car", 50000.00, 5);
